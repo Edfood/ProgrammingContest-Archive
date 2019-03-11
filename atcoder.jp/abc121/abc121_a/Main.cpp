b801bc64fc137d1f0d47112e82c21538fbd4545b
ScottShelby <kfed542@gmail.com>
2019-03-11 18:39:53 +0900

    [AC] abc121 abc121_a

---
+++ atcoder.jp/abc121/abc121_a/Main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<utility>
+#include<cmath>
+#include<climits>
+
+using namespace std;
+
+
+int main(){
+  int H, W, h, w;
+  cin >> H >> W >> h >> w;
+  cout << (H - h) * (W - w) <<endl;
+}
