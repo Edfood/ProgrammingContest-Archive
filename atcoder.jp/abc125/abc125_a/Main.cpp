85dddc1dfa5b6cff10873235e80ea5128db01487
ScottShelby <kfed542@gmail.com>
2019-05-19 13:12:33 +0900

    [AC] abc125 abc125_a

---
+++ atcoder.jp/abc125/abc125_a/Main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<utility>
+#include<cmath>
+#include<climits>
+#include<queue>
+using namespace std;
+
+
+int main(){
+  int a, b, t;
+  int ans;
+  cin >> a >> b >> t;
+
+  ans = t / a * b;
+
+  cout << ans << endl;
+
+}
