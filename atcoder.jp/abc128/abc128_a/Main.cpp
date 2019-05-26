a45d844495de4ac48347a3dde545c24b9addb845
ScottShelby <kfed542@gmail.com>
2019-05-26 21:02:57 +0900

    [AC] abc128 abc128_a

---
+++ atcoder.jp/abc128/abc128_a/Main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<utility>
+#include<cmath>
+#include<climits>
+#include<queue>
+using namespace std;
+
+
+int main(){
+  int a, p;
+  cin >> a >> p;
+  cout << ((a * 3) + p )/ 2 << endl;
+
+}
