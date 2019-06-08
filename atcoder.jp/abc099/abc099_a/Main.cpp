2eff708503a818a78510880aaa53f7d81fa6aa56
ScottShelby <kfed542@gmail.com>
2019-06-09 00:35:36 +0900

    [AC] abc099 abc099_a

---
+++ atcoder.jp/abc099/abc099_a/Main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<utility>
+#include<cmath>
+#include<climits>
+#include<queue>
+#include<stack>
+#include<numeric>
+using namespace std;
+
+typedef long long ll;
+#define rep(i,n) for(int (i)=0;(i)<(n);(i)++)
+
+
+int main(){
+  int n;
+  cin >> n;
+  if(n <= 999)  cout << "ABC" << endl;
+  else cout << "ABD" << endl;
+
+
+}
