a8c4f7da6e3ea70af1048706201eb4ea1a4694b5
ScottShelby <kfed542@gmail.com>
2019-06-16 21:01:18 +0900

    [AC] abc130 abc130_a

---
+++ atcoder.jp/abc130/abc130_a/Main.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<utility>
+#include<cmath>
+#include<climits>
+#include<queue>
+#include<stack>
+#include<numeric>
+#include<set>
+#include<iomanip>
+using namespace std;
+
+
+typedef long long ll;
+#define rep(i,n) for(int (i)=0;(i)<(n);(i)++)
+
+
+int main(){
+  int x, a;
+  cin >> x >> a;
+  if(x < a){
+    cout << 0 << endl;
+  } else {
+    cout << 10 << endl;
+  }
+
+}
