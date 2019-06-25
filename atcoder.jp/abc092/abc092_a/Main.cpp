09679956bd2ac5961e423c9e90f3533deee7fa08
ScottShelby <kfed542@gmail.com>
2019-06-25 17:00:01 +0900

    [AC] abc092 abc092_a

---
+++ atcoder.jp/abc092/abc092_a/Main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<utility>
+#include<cmath>
+#include<climits>
+#include<queue>
+#include<stack>
+#include<numeric>
+#include<set>
+#include<iomanip>
+using namespace std;
+
+
+typedef long long ll;
+#define rep(i,n) for(int (i)=0;(i)<(n);(i)++)
+
+
+int main(){
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+  int ans;
+  ans = min(a, b);
+  ans += min(c, d);
+  cout << ans << endl;
+}
