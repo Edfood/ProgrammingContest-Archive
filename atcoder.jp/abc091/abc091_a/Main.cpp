66611d089185a01175d97dd83f1e5223fed0c6b9
ScottShelby <kfed542@gmail.com>
2019-07-04 22:21:43 +0900

    [AC] abc091 abc091_a

---
+++ atcoder.jp/abc091/abc091_a/Main.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<utility>
+#include<cmath>
+#include<climits>
+#include<queue>
+#include<stack>
+#include<numeric>
+#include<set>
+#include<iomanip>
+using namespace std;
+
+
+typedef long long ll;
+#define rep(i,n) for(int (i)=0;(i)<(n);(i)++)
+
+
+
+int main(){
+  int a, b, c;
+  cin >> a >> b >> c;
+  if(a + b >= c){
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+}
