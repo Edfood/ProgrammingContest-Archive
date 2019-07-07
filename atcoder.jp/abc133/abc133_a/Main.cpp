22ef849311fc5d0d04644302e0933b3872ae0ae6
ScottShelby <kfed542@gmail.com>
2019-07-08 00:23:21 +0900

    [AC] abc133 abc133_a

---
+++ atcoder.jp/abc133/abc133_a/Main.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<utility>
+#include<cmath>
+#include<climits>
+#include<queue>
+#include<stack>
+#include<numeric>
+#include<set>
+#include<iomanip>
+using namespace std;
+
+
+typedef long long ll;
+#define rep(i,n) for(int (i)=0;(i)<(n);(i)++)
+
+
+
+int main(){
+  int n, a, b;
+  cin >> n >> a >> b;
+  int ans = min(a * n, b);
+  cout << ans << endl;
+}
