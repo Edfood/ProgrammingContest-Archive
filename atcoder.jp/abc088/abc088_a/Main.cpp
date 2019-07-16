a09a2495fcc1654bbd4da80ccc3666688ab7a644
ScottShelby <kfed542@gmail.com>
2019-07-17 03:06:13 +0900

    [AC] abc088 abc088_a

---
+++ atcoder.jp/abc088/abc088_a/Main.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<utility>
+#include<cmath>
+#include<climits>
+#include<queue>
+#include<stack>
+#include<numeric>
+#include<set>
+#include<iomanip>
+#include<map>
+using namespace std;
+
+
+typedef long long ll;
+#define rep(i,n) for(int (i)=0;(i)<(n);(i)++)
+
+
+int main(){
+  int n, a;
+  cin >> n >> a;
+  if (n % 500 <= a) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+
+
+}
