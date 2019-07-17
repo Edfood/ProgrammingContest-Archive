822eec327428d142efd9ec984a0b5dffe838e4f8
ScottShelby <kfed542@gmail.com>
2019-07-18 02:16:17 +0900

    [AC] abc087 abc087_a

---
+++ atcoder.jp/abc087/abc087_a/Main.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<utility>
+#include<cmath>
+#include<climits>
+#include<queue>
+#include<stack>
+#include<numeric>
+#include<set>
+#include<iomanip>
+#include<map>
+using namespace std;
+
+
+typedef long long ll;
+#define rep(i,n) for(int (i)=0;(i)<(n);(i)++)
+
+
+
+int main(){
+  int x, a, b;
+  cin >> x >> a >> b;
+
+  cout << (x - a) % b << endl;
+
+}
