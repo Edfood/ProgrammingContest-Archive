5bc1ca4d265e9ff48eb4c56848e58f00ffffb919
ScottShelby <kfed542@gmail.com>
2019-08-09 19:53:06 +0900

    [AC] abc078 abc078_a

---
+++ atcoder.jp/abc078/abc078_a/Main.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<utility>
+#include<cmath>
+#include<climits>
+#include<queue>
+#include<stack>
+#include<numeric>
+#include<set>
+#include<iomanip>
+#include<map>
+#include<type_traits>
+using namespace std;
+
+
+typedef long long ll;
+#define rep(i,N) for(ll (i)=0;(i)<(N);(i)++)
+
+const int mod = 1000000007;
+
+
+int main(){
+
+char x, y;
+cin >> x >> y;
+char ans;
+if(x > y) ans = '>';
+else if(x == y) ans = '=';
+else ans = '<';
+cout << ans << endl;
+}
