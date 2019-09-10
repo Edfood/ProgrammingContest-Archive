51eb4eceb5cd1566838fce164094f42a2b4e5903
ScottShelby <kfed542@gmail.com>
2019-09-10 16:32:32 +0900

    [AC] abc139 abc139_a

---
+++ atcoder.jp/abc139/abc139_a/Main.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<utility>
+#include<cmath>
+#include<climits>
+#include<queue>
+#include<stack>
+#include<numeric>
+#include<set>
+#include<iomanip>
+#include<map>
+#include<type_traits>
+using namespace std;
+
+
+typedef long long ll;
+#define rep(i,N) for(ll (i)=0;(i)<(N);(i)++)
+
+const int mod = 1000000007;
+
+
+int main(){
+  string s, t;
+  cin >> s;
+  cin >> t;
+
+  int ans = 0;
+  rep(i, 3){
+    if(s[i] == t[i]) ans++;
+  }
+  cout << ans << endl;
+}
