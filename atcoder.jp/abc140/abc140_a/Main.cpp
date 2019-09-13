726c9e3b7152eabf0bed898102beb92acbb8cc6a
ScottShelby <kfed542@gmail.com>
2019-09-13 22:01:14 +0900

    [AC] abc140 abc140_a

---
+++ atcoder.jp/abc140/abc140_a/Main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<utility>
+#include<cmath>
+#include<climits>
+#include<queue>
+#include<stack>
+#include<numeric>
+#include<set>
+#include<iomanip>
+#include<map>
+#include<type_traits>
+using namespace std;
+
+
+typedef long long ll;
+#define rep(i,N) for(ll (i)=0;(i)<(N);(i)++)
+
+const int mod = 1000000007;
+
+int main(){
+  int n;
+  cin >> n;
+  printf("%d\n", n * n * n);
+}
