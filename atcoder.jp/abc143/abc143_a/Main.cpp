139f55ae3b5605fad21b7a561c71cfafe60cc8a5
ScottShelby <kfed542@gmail.com>
2020-02-09 17:55:25 +0900

    [AC] abc143 abc143_a

---
+++ atcoder.jp/abc143/abc143_a/Main.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<utility>
+#include<cmath>
+#include<climits>
+#include<queue>
+#include<stack>
+#include<numeric>
+#include<tuple>
+#include<cassert>
+using namespace std;
+typedef pair<int, int> P;
+typedef long long ll;
+
+#define rep(i,n) for(int (i)=0;(i)<(n);(i)++)
+ll gcd(ll a, ll b) { return b?gcd(b,a%b):a;}
+ll lcm(ll a, ll b) { return a/gcd(a,b)*b;}
+
+const int mod = pow(10, 9) + 7;
+
+int main(){
+    int a, b;
+    cin >> a >> b;
+    b *= 2;
+    if(a - b >= 0){
+        cout << a-b << endl;
+    } else {
+        cout << 0 << endl;
+    }
+}
