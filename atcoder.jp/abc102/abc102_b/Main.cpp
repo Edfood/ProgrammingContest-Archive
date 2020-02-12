9bd8fe4997932f4ac10d976290a59ca9fa8551b9
ScottShelby <kfed542@gmail.com>
2020-02-12 12:49:37 +0900

    [AC] abc102 abc102_b

---
+++ atcoder.jp/abc102/abc102_b/Main.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<utility>
+#include<cmath>
+#include<climits>
+#include<queue>
+#include<stack>
+#include<numeric>
+#include<set>
+#include<iomanip>
+#include<map>
+#include<type_traits>
+#include<tuple>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+#define rep(i,N) for(ll (i)=0;(i)<(N);(i)++)
+const int mod = 1000000007;
+
+
+int main(){
+	int n;
+	cin >> n;
+	vector<ll> a(n);
+
+	rep(i, n)	cin >> a[i];
+	sort(a.begin(), a.end());
+	cout << abs(a[0] - a[n - 1]) << endl;
+}
