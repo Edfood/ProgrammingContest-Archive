f6a87e0b46d3fdd6c646f3f35345fb73a2cf07fe
ScottShelby <kfed542@gmail.com>
2020-02-17 16:14:53 +0900

    [AC] abc070 abc070_c

---
+++ atcoder.jp/abc070/abc070_c/Main.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<utility>
+#include<cmath>
+#include<climits>
+#include<queue>
+#include<stack>
+#include<numeric>
+#include<set>
+#include<iomanip>
+#include<map>
+#include<type_traits>
+#include<tuple>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+#define rep(i,N) for(ll (i)=0;(i)<(N);(i)++)
+const int mod = 1000000007;
+
+ll gcd(ll a, ll b) { return b?gcd(b,a%b):a;}
+ll lcm(ll a, ll b) { return a/gcd(a,b)*b;}
+int main(){
+  ll n;
+  cin >> n;
+  vector<ll> t(n);
+  ll ans = 1;
+  rep(i, n) cin >> t[i];
+  for(int i = 0; i < n; ++i){
+    ans = lcm(ans, t[i]);
+  }
+  cout << ans << endl;
+}
