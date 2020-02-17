6856d97f536d4f35d3b7673b0c08618197a72a74
ScottShelby <kfed542@gmail.com>
2020-02-17 15:20:33 +0900

    [AC] abc071 abc071_a

---
+++ atcoder.jp/abc071/abc071_a/Main.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<utility>
+#include<cmath>
+#include<climits>
+#include<queue>
+#include<stack>
+#include<numeric>
+#include<set>
+#include<iomanip>
+#include<map>
+#include<type_traits>
+#include<tuple>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+#define rep(i,N) for(ll (i)=0;(i)<(N);(i)++)
+const int mod = 1000000007;
+
+int main(){
+  int x, a, b;
+  cin >> x >> a >> b;
+  int da = abs(x - a);
+  int db = abs(x - b);
+  if(da < db) cout << "A" << endl;
+  else cout << "B" << endl;
+}
