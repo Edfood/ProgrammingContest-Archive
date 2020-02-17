159c11010a938099942e094a37b7167ecd2c3fc1
ScottShelby <kfed542@gmail.com>
2020-02-17 15:51:09 +0900

    [AC] abc070 abc070_a

---
+++ atcoder.jp/abc070/abc070_a/Main.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<utility>
+#include<cmath>
+#include<climits>
+#include<queue>
+#include<stack>
+#include<numeric>
+#include<set>
+#include<iomanip>
+#include<map>
+#include<type_traits>
+#include<tuple>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+#define rep(i,N) for(ll (i)=0;(i)<(N);(i)++)
+const int mod = 1000000007;
+
+int main(){
+  int n;
+  cin >> n;
+  int a, b;
+  a = n % 10;
+  b = (n / 100);
+  if(a == b)  cout << "Yes" << endl;
+  else cout << "No" << endl;
+}
