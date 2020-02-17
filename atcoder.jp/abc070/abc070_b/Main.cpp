f9047cd59fb849f16f7eeb2ee8d4d6aa2c72227d
ScottShelby <kfed542@gmail.com>
2020-02-17 16:02:32 +0900

    [AC] abc070 abc070_b

---
+++ atcoder.jp/abc070/abc070_b/Main.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<utility>
+#include<cmath>
+#include<climits>
+#include<queue>
+#include<stack>
+#include<numeric>
+#include<set>
+#include<iomanip>
+#include<map>
+#include<type_traits>
+#include<tuple>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+#define rep(i,N) for(ll (i)=0;(i)<(N);(i)++)
+const int mod = 1000000007;
+
+int main(){
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+  int lower = max(a, c);
+  int upper = min (b, d);
+  int ans = max(0, upper - lower);
+  cout << ans << endl;
+}
