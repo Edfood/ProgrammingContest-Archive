3b59836bb4b88b50e39299f8015d553d000f39a0
ScottShelby <kfed542@gmail.com>
2020-02-27 22:21:42 +0900

    [AC] abc045 abc045_a

---
+++ atcoder.jp/abc045/abc045_a/Main.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+#define rep(i,N) for(ll (i)=0;(i)<(N);(i)++)
+const int mod = 1000000007;
+
+int main(){
+  int a, b , h;
+  cin >> a >> b >> h;
+  cout << (a + b) * h / 2 << endl;
+}
