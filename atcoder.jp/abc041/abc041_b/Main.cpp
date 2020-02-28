a0fe7dc4e5c56fb27e752556d0681b28eff692b5
ScottShelby <kfed542@gmail.com>
2020-02-29 06:15:59 +0900

    [AC] abc041 abc041_b

---
+++ atcoder.jp/abc041/abc041_b/Main.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+#define rep(i,N) for(ll (i)=0;(i)<(N);(i)++)
+const int mod = 1000000007;
+
+int main(){
+  ll a, b, c;
+  cin >> a >> b >> c;
+  a *= b;
+  a %= mod;
+  a *= c;
+  a %= mod;
+  cout << a << endl;
+}
