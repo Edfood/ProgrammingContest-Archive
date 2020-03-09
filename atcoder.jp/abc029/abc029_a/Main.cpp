ef1500e157587018f379a0b59e4a671498c550d0
ScottShelby <kfed542@gmail.com>
2020-03-09 15:58:14 +0900

    [AC] abc029 abc029_a

---
+++ atcoder.jp/abc029/abc029_a/Main.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+#define rep(i,N) for(ll (i)=0;(i)<(N);(i)++)
+const int mod = 1000000007;
+
+int main(){
+  string w;
+  cin >> w;
+  w += "s";
+  cout << w << endl;
+}
