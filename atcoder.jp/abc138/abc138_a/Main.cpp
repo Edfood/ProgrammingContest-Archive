79acd0a3d4a98731deace8a1ea1954a00a59c2ee
ScottShelby <kfed542@gmail.com>
2020-03-13 12:26:01 +0900

    [AC] abc138 abc138_a

---
+++ atcoder.jp/abc138/abc138_a/Main.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+#define rep(i,N) for(ll (i)=0;(i)<(N);(i)++)
+const int mod = 1000000007;
+
+int main(){
+  int a;
+  string s;
+  cin >> a >> s;
+  if (a >= 3200) cout << s << endl;
+  else cout << "red" << endl;
+}
