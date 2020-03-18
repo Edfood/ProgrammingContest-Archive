4126d38b316b16b08dbf9eacb7c4eda00dc879fc
ScottShelby <kfed542@gmail.com>
2020-03-19 03:12:22 +0900

    [AC] abc034 abc034_b

---
+++ atcoder.jp/abc034/abc034_b/Main.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+#define rep(i,N) for(ll (i)=0;(i)<(N);(i)++)
+const int mod = 1000000007;
+
+int main(){
+  int n;
+  cin >> n;
+  if (n % 2) cout << n + 1 << endl;
+  else cout << n - 1 << endl;
+}
