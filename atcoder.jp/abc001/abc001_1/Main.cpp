e07e3a7db25256e09f2ef90a8d674eca13036656
ScottShelby <kfed542@gmail.com>
2020-03-21 00:20:25 +0900

    [AC] abc001 abc001_1

---
+++ atcoder.jp/abc001/abc001_1/Main.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+#define rep(i,N) for(ll (i)=0;(i)<(N);(i)++)
+const int mod = 1000000007;
+
+int main(){
+  int h1, h2;
+  cin >> h1 >> h2;
+  cout << h1 - h2 << endl;
+}
