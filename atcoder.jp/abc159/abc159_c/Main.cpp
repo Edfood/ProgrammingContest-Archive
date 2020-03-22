91602fa4e261b558644856563e6c6c1b968f63ad
ScottShelby <kfed542@gmail.com>
2020-03-22 21:21:11 +0900

    [AC] abc159 abc159_c

---
+++ atcoder.jp/abc159/abc159_c/Main.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+#define rep(i,N) for(ll (i)=0;(i)<(N);(i)++)
+const int mod = 1000000007;
+
+int main(){
+  long double l;
+  cin >> l;
+  l /= 3;
+  printf("%.6Lf\n", l * l * l);
+}
