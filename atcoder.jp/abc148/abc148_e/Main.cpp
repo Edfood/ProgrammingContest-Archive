69a7ef0815bda630546b2b4f089b008f4510351e
ScottShelby <kfed542@gmail.com>
2020-04-02 01:22:00 +0900

    [AC] abc148 abc148_e

---
+++ atcoder.jp/abc148/abc148_e/Main.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<utility>
+#include<cmath>
+#include<climits>
+#include<queue>
+#include<stack>
+#include<numeric>
+#include<set>
+#include<iomanip>
+#include<map>
+#include<type_traits>
+#include<tuple>
+#include<deque>
+#include<cassert>
+using namespace std;
+typedef long long ll;
+typedef pair<ll,int> P;
+#define rep(i,N) for(ll (i)=0;(i)<(N);(i)++)
+const int mod = 1000000007;
+const int INF = 1001001001;
+
+ll gcd(ll a, ll b) { return b?gcd(b,a%b):a;}
+ll lcm(ll a, ll b) { return a/gcd(a,b)*b;}
+
+int main(){
+  ll n;
+  cin >> n;
+  if(n % 2) {
+    puts("0");
+    return 0;
+  }
+  ll ans = 0;
+  for(ll i = 10; i <= n; i *= 5) {
+    ans += n / i;
+  }
+  cout << ans << endl;
+
+}
