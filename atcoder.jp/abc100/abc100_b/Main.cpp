8c31ca8f9307f9c760abf1e3f7f0e5e58bed6be2
ScottShelby <kfed542@gmail.com>
2020-04-04 09:06:40 +0900

    [AC] abc100 abc100_b

---
+++ atcoder.jp/abc100/abc100_b/Main.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<utility>
+#include<cmath>
+#include<climits>
+#include<queue>
+#include<stack>
+#include<numeric>
+#include<set>
+#include<iomanip>
+#include<map>
+#include<type_traits>
+#include<tuple>
+#include<deque>
+#include<cassert>
+using namespace std;
+typedef long long ll;
+typedef pair<ll,int> p;
+#define rep(i,N) for(ll (i)=0;(i)<(N);(i)++)
+const int mod = 1000000007;
+const int INF = 1001001001;
+
+int main() {
+  int d, n;
+  cin >> d >> n;
+  int ans;
+  if(n == 100) {
+    ans = n * pow(100, d) + pow(100, d);
+  } else {
+    ans = n * pow(100, d);
+  }
+  cout << ans << endl;
+}
