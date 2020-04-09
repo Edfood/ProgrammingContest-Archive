5220aee4a61e76b322b7f4240c3f707669017e52
ScottShelby <kfed542@gmail.com>
2020-04-09 16:01:02 +0900

    [AC] abc048 abc048_a

---
+++ atcoder.jp/abc048/abc048_a/Main.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<utility>
+#include<cmath>
+#include<climits>
+#include<queue>
+#include<stack>
+#include<numeric>
+#include<set>
+#include<iomanip>
+#include<map>
+#include<type_traits>
+#include<tuple>
+#include<deque>
+#include<cassert>
+using namespace std;
+typedef long long ll;
+typedef pair<ll,int> P;
+#define rep(i,N) for(ll (i)=0;(i)<(N);(i)++)
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) y=min(x,y)
+const int mod = 1000000007;
+const int INF = 1001001001;
+
+int main() {
+  string a, b, c;
+  cin >> a >> b >> c;
+  cout << a[0] << b[0] << c[0] << endl;
+}
