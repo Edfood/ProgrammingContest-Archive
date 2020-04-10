d3448ed09ff5530ebb342d5abdbce6982d3d79c8
ScottShelby <kfed542@gmail.com>
2020-04-10 17:53:32 +0900

    [AC] abc039 abc039_a

---
+++ atcoder.jp/abc039/abc039_a/Main.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<utility>
+#include<cmath>
+#include<climits>
+#include<queue>
+#include<stack>
+#include<numeric>
+#include<set>
+#include<iomanip>
+#include<map>
+#include<type_traits>
+#include<tuple>
+#include<deque>
+#include<cassert>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+#define rep(i,N) for(ll (i)=0;(i)<(N);(i)++)
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+const int mod = 1000000007;
+const int INF = 1001001001;
+
+int main() {
+  int a, b, c;
+  cin >> a >> b >> c;
+  int ans = 2 * (a*b + b*c + c*a);
+  cout << ans << endl;
+}
