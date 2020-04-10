1ae0030bc8be453fab96947c4fcc6457eabfcc5b
ScottShelby <kfed542@gmail.com>
2020-04-10 18:11:20 +0900

    [AC] abc144 abc144_b

---
+++ atcoder.jp/abc144/abc144_b/Main.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<utility>
+#include<cmath>
+#include<climits>
+#include<queue>
+#include<stack>
+#include<numeric>
+#include<set>
+#include<iomanip>
+#include<map>
+#include<type_traits>
+#include<tuple>
+#include<deque>
+#include<cassert>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+#define rep(i,N) for(ll (i)=0;(i)<(N);(i)++)
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+const int mod = 1000000007;
+const int INF = 1001001001;
+
+int main() {
+  int n;
+  cin >> n;
+  for(int i = 1; i <= 9; ++i) {
+    if(n % i == 0 && n / i <= 9) {
+      cout << "Yes" << endl;
+      return 0;
+    }
+  }
+  cout << "No" << endl;
+}
