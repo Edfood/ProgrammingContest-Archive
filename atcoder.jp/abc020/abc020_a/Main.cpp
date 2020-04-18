523a8b1df00fbc4c59a76d34ff672d11e3e6fec7
ScottShelby <kfed542@gmail.com>
2020-04-18 12:53:15 +0900

    [AC] abc020 abc020_a

---
+++ atcoder.jp/abc020/abc020_a/Main.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define rep(i,N) for(ll (i)=0;(i)<(N);(i)++)
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+const int mod = 1000000007;
+
+int main() {
+  string a = "ABC";
+  string b = "chokudai";
+  int q1;
+  cin >> q1;
+  if(q1 == 1) cout << a << endl;
+  else cout << b << endl;
+}
