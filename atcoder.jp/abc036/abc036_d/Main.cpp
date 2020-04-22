b4dead59e7d1e97feb64247f1d584752c83e5728
ScottShelby <kfed542@gmail.com>
2020-04-22 22:07:06 +0900

    [AC] abc036 abc036_d

---
+++ atcoder.jp/abc036/abc036_d/Main.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+#define rep(i,N) for(ll (i)=0;(i)<(N);(i)++)
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+using namespace std;
+typedef long long ll;
+
+const int mod = 1000000007;
+
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod-a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const { return pow(mod-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+typedef pair<mint,mint> P;
+vector<int> to[100100];
+
+P dfs(int v, int pre) {
+  mint white = 1, black = 1;
+  for (int u : to[v]) {
+    if(u == pre) continue;
+    auto t = dfs(u, v);
+    white *= (t.first + t.second);
+    black *= t.first;
+  }
+  return P(white, black);
+}
+
+int main() {
+  int n;
+  cin >> n;
+  rep(i, n - 1) {
+    int a, b;
+    cin >> a >> b;
+    --a; --b;
+    to[a].push_back(b);
+    to[b].push_back(a);
+  }
+
+  auto res = dfs(0, -1);
+  mint ans = res.first + res.second;
+  cout << ans << endl;
+
+}
