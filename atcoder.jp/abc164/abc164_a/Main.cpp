d3b42f14e80f2d7ead08b009688e7411024d85ff
ScottShelby <kfed542@gmail.com>
2020-04-26 21:01:27 +0900

    [AC] abc164 abc164_a

---
+++ atcoder.jp/abc164/abc164_a/Main.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define rep(i,N) for(ll (i)=0;(i)<(N);(i)++)
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+const int mod = 1000000007;
+
+int main() {
+  int s, w;
+  cin >> s >> w;
+  if(w >= s) cout << "unsafe" << endl;
+  else cout << "safe" << endl;
+}
