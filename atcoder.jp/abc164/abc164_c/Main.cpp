8c10638158552e2c83ed705e16a0b639eb24e33f
ScottShelby <kfed542@gmail.com>
2020-04-26 21:07:46 +0900

    [AC] abc164 abc164_c

---
+++ atcoder.jp/abc164/abc164_c/Main.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define rep(i,N) for(ll (i)=0;(i)<(N);(i)++)
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+const int mod = 1000000007;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<string> s(n);
+  rep(i, n) {
+    cin >> s[i];
+  }
+  set<string> st;
+  rep(i, n) {
+    st.insert(s[i]);
+  }
+  cout << st.size() << endl;
+}
