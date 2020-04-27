80517ce42972a5da0e41888646dea0f1b03856c5
ScottShelby <kfed542@gmail.com>
2020-04-27 17:08:15 +0900

    [AC] abc161 abc161_c

---
+++ atcoder.jp/abc161/abc161_c/Main.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<utility>
+#include<cmath>
+#include<climits>
+#include<queue>
+#include<stack>
+#include<numeric>
+#include<set>
+#include<iomanip>
+#include<map>
+#include<type_traits>
+#include<tuple>
+#include<deque>
+#include<cassert>
+#include<bitset>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+#define rep(i,N) for(ll (i)=0;(i)<(N);(i)++)
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+const int mod = 1000000007;
+
+int main() {
+  ll n, k;
+  cin >> n >> k;
+  ll remain = n % k;
+  ll ans = min(remain, abs(remain - k));
+  cout << ans << endl;
+}
