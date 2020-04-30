8839182174bf3e0be772f0bef88aa6e1017463ed
ScottShelby <kfed542@gmail.com>
2020-04-30 10:38:50 +0900

    [AC] abc019 abc019_4

---
+++ atcoder.jp/abc019/abc019_4/Main.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<utility>
+#include<cmath>
+#include<climits>
+#include<queue>
+#include<stack>
+#include<numeric>
+#include<set>
+#include<iomanip>
+#include<map>
+#include<type_traits>
+#include<tuple>
+#include<deque>
+#include<cassert>
+#include<bitset>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+#define rep(i,N) for(ll (i)=0;(i)<(N);(i)++)
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+const int mod = 1000000007;
+
+int main() {
+  int n;
+  cin >> n;
+  int maxd = 0;
+  int idx = 0;
+  rep(i, n - 2) {
+    
+    cout << "? " << n << " " << i + 1 << endl;
+    int t;
+    cin >> t;
+    if (maxd < t) {
+      maxd = t;
+      idx = i;
+    }
+  }
+  int ans = 0;
+  rep(i, n) {
+    if(i == idx) continue;
+    cout << "? " << idx + 1 << " " << i + 1 << endl;
+    int t;
+    cin >> t;
+    chmax(ans, t);
+  }
+
+  cout << "! " << ans << endl;
+}
