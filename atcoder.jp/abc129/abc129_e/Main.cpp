e79d33e8190d1be149780833ef51cbe5a16d7d00
ScottShelby <kfed542@gmail.com>
2020-05-02 02:14:39 +0900

    [AC] abc129 abc129_e

---
+++ atcoder.jp/abc129/abc129_e/Main.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<utility>
+#include<cmath>
+#include<climits>
+#include<queue>
+#include<stack>
+#include<numeric>
+#include<set>
+#include<iomanip>
+#include<map>
+#include<type_traits>
+#include<tuple>
+#include<deque>
+#include<cassert>
+#include<bitset>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+#define rep(i,N) for(ll (i)=0;(i)<(N);(i)++)
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+const int mod = 1000000007;
+
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod-a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const { return pow(mod-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+mint dp[100010][2];
+
+int main() {
+  string s;
+  cin >> s;
+  int n = s.length();
+  dp[0][0] = 1;
+  rep(i, n)rep(j, 2) {
+    int ni = i + 1;
+    int nj = j;
+    if (s[i] == '1') {
+      if (nj == 0) {
+        dp[ni][nj] += dp[i][j] * 2;
+        dp[ni][1] += dp[i][j]; 
+      } else {
+        dp[ni][nj] += dp[i][j] * 3;
+      }
+    } else {
+      if (nj == 1) dp[ni][nj] += dp[i][j] * 3;
+      else dp[ni][nj] += dp[i][j];
+    }
+  }
+  cout << dp[n][0] + dp[n][1] << endl;
+}
