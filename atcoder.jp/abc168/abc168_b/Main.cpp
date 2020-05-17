60db7368bcef48174626ac1cde39187c610bf533
ScottShelby <kfed542@gmail.com>
2020-05-17 21:05:40 +0900

    [AC] abc168 abc168_b

---
+++ atcoder.jp/abc168/abc168_b/Main.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define rep(i,N) for(ll (i)=0;(i)<(N);(i)++)
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+const int mod = 1000000007;
+
+int main() {
+  int k;
+  cin >> k;
+  string s;
+  cin >> s;
+  if (s.length() <= k) {
+    cout << s << endl;
+  } else {
+    rep(i, k) cout << s[i];
+    cout << "..." << endl;
+  }
+}
