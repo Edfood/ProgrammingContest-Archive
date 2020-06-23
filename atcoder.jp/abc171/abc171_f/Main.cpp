d6f40446b4c6f570e3c0bbf0b5bf05ce36ca3ca8
ScottShelby <kfed542@gmail.com>
2020-06-23 21:56:16 +0900

    [AC] abc171 abc171_f

---
+++ atcoder.jp/abc171/abc171_f/Main.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+#define rep(i,N) for(ll (i)=0;(i)<(N);(i)++)
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+const int mod = 1000000007;
+
+struct mint {
+  ll x; // typedef long long ll;
+  mint(ll x=0):x((x%mod+mod)%mod){}
+  mint operator-() const { return mint(-x);}
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod-a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this;}
+  mint operator+(const mint a) const { return mint(*this) += a;}
+  mint operator-(const mint a) const { return mint(*this) -= a;}
+  mint operator*(const mint a) const { return mint(*this) *= a;}
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t>>1);
+    a *= a;
+    if (t&1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const { return pow(mod-2);}
+  mint& operator/=(const mint a) { return *this *= a.inv();}
+  mint operator/(const mint a) const { return mint(*this) /= a;}
+};
+istream& operator>>(istream& is, const mint& a) { return is >> a.x;}
+ostream& operator<<(ostream& os, const mint& a) { return os << a.x;}
+
+
+struct combination {
+  vector<mint> fact, ifact;
+  combination(int n):fact(n+1),ifact(n+1) {
+    assert(n < mod);
+    fact[0] = 1;
+    for (int i = 1; i <= n; ++i) fact[i] = fact[i-1]*i;
+    ifact[n] = fact[n].inv();
+    for (int i = n; i >= 1; --i) ifact[i-1] = ifact[i]*i;
+  }
+  mint operator()(int n, int k) {
+    if (k < 0 || k > n) return 0;
+    return fact[n]*ifact[k]*ifact[n-k];
+  }
+};
+
+int main() {
+  int k;
+  string s;
+  cin >> k >> s;
+  int n = s.size();
+  combination comb(n + k);
+
+  // when s = "abc" and k = 2, the result will be as follows: "○a○b○c○"
+  // the combination of the rightmost section (the right side of "c") is 26^(k - i) ways.
+  // the combination of the other section is 25^(k) * (k + n - 1)Comb(i)
+  mint ans = 0;
+  rep(i, k + 1) {
+    ans += mint(25).pow(i) * comb(n + i - 1, i) * mint(26).pow(k - i);
+  }
+  
+  cout << ans << endl;
+}
