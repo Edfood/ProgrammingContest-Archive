8c1a44b728be844c264d7d721469e44956f76e8d
ScottShelby <kfed542@gmail.com>
2020-07-05 21:01:04 +0900

    [AC] abc173 abc173_a

---
+++ atcoder.jp/abc173/abc173_a/Main.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define rep(i,N) for(ll (i)=0;(i)<(N);(i)++)
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+const int mod = 1000000007;
+
+int main() {
+  int n;
+  cin >> n;
+  int ans = (1000 - n % 1000) % 1000;
+  cout << ans << endl;
+}
