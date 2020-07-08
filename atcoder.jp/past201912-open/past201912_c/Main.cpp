565f55953d7156ae7245149e0ab47ace8c1106f5
ScottShelby <kfed542@gmail.com>
2020-07-08 18:50:54 +0900

    [AC] past201912-open past201912_c

---
+++ atcoder.jp/past201912-open/past201912_c/Main.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<utility>
+#include<cmath>
+#include<climits>
+#include<queue>
+#include<stack>
+#include<numeric>
+#include<set>
+#include<iomanip>
+#include<map>
+#include<type_traits>
+#include<tuple>
+#include<deque>
+#include<cassert>
+#include<bitset>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> P;
+#define rep(i,N) for(int (i)=0;(i)<(N);(i)++)
+#define chmax(x,y) x=max(x,y)
+#define chmin(x,y) x=min(x,y)
+const double PI = 3.1415926535897932384626433832795;
+const int mod = 1000000007;
+
+int main() {
+  vector<int> a(6);
+  rep(i, 6) cin >> a[i];
+  sort(a.rbegin(), a.rend());
+  cout << a[2] << endl;
+}
